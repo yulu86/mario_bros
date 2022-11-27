0af24b1ee9c687093aafbe80b14618d1ad176216
Yulu Xu <xuyulu86@126.com>
2022-11-27 17:33:13 +0800

    load images

--- lib/src/klondike_game.dart
+++ lib/src/klondike_game.dart
@@ -1,8 +1,17 @@
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 
 class KlondikeGame extends FlameGame {
   @override
   Future<void>? onLoad() async {
     var image = await images.load('klondike-sprites.png');
+  }
+
+  Sprite kondikeSprite(double x, double y, double width, double height) {
+    return Sprite(
+      images.fromCache('klondike-sprites.png'),
+      srcPosition: Vector2(x, y),
+      srcSize: Vector2(width, height),
+    );
   }
 }
