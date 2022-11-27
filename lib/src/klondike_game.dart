929d5d63dff98f686a77e6ec137205b30a27604e
Yulu Xu <xuyulu86@126.com>
2022-11-27 18:02:11 +0800

    add components

--- lib/src/klondike_game.dart
+++ lib/src/klondike_game.dart
@@ -1,10 +1,59 @@
 import 'package:flame/components.dart';
+import 'package:flame/experimental.dart';
 import 'package:flame/game.dart';
+import 'package:mario_bros/src/components/foundation.dart';
+import 'package:mario_bros/src/components/pile.dart';
+import 'package:mario_bros/src/components/stock.dart';
+import 'package:mario_bros/src/components/waste.dart';
 
 class KlondikeGame extends FlameGame {
+  // 卡片大小
+  static const double cardWidth = 1000;
+  static const double cardHeight = 1400;
+  // 卡片间隔
+  static const double cardGap = 175;
+  static const double cardRadius = 100;
+  static final Vector2 cardSize = Vector2(cardWidth, cardHeight);
+
   @override
   Future<void>? onLoad() async {
     var image = await images.load('klondike-sprites.png');
+
+    final stock = Stock()
+      ..size = cardSize
+      ..position = Vector2(cardGap, cardGap);
+
+    final waste = Waste()
+      ..size = cardSize
+      ..position = Vector2(cardWidth + 2 * cardGap, cardGap);
+
+    final foundations = List.generate(
+      4,
+      (index) => Foundation()
+        ..size = cardSize
+        ..position = Vector2(
+          (index + 3) * (cardWidth + cardGap) + cardGap,
+          cardGap,
+        ),
+    );
+
+    final piles = List.generate(
+      7,
+      (index) => Pile()
+        ..size = cardSize
+        ..position = Vector2(
+          cardGap + index * (cardWidth + cardGap),
+          cardHeight + 2 * cardGap,
+        ),
+    );
+
+    final world = World()
+      ..add(stock)
+      ..add(waste)
+      ..addAll(foundations)
+      ..addAll(piles);
+
+    add(world);
   }
 
   Sprite kondikeSprite(double x, double y, double width, double height) {
