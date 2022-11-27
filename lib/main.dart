0cbb717b2459010f18885d8f18fce4d3f8aec89d
Yulu Xu <xuyulu86@126.com>
2022-11-27 17:47:17 +0800

    add components

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:mario_bros/src/klondike_game.dart';
 
 void main() {
-  runApp(MarioBrosGame());
-}
-
-class MarioBrosGame extends StatelessWidget {
-  MarioBrosGame({Key? key}) : super(key: key);
-
-  final FlameGame _game = KlondikeGame();
-
-  @override
-  Widget build(BuildContext context) {
-    return GameWidget(game: _game);
-  }
+  final game = KlondikeGame();
+  runApp(GameWidget(game: game));
 }
