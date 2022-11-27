84c103a8a9c43434a031668a9cecb10cc7a5213d
Yulu Xu <xuyulu86@126.com>
2022-11-27 23:00:28 +0800

    add mario sprite

--- lib/src/mario_game.dart
+++ lib/src/mario_game.dart
@@ -1,25 +1,57 @@
 import 'dart:ui';
 
 import 'package:flame/components.dart';
+import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flame/sprite.dart';
 
 class MarioGame extends FlameGame {
-  late Image marioImage;
-
   @override
   Future<void>? onLoad() async {
-    marioImage = await images.load('mario_sprites.gif');
+    await images.load(spriteImage);
 
-    final upSprite = await loadSprite(
-      'mario_sprites.gif',
-      srcSize: Vector2(680, 1252),
-      srcPosition: Vector2(0, 0),
-    );
+    addAll([
+      SpriteComponent(
+        sprite: getMarioSprite(x: 52, y: 43, width: 16, height: 16),
+        position: Vector2(150, 50),
+        size: Vector2(32, 32),
+      ),
+      SpriteComponent(
+        sprite: getMarioSprite(x: 71, y: 43, width: 16, height: 16),
+        position: Vector2(200, 50),
+        size: Vector2(32, 32),
+      ),
+    ]);
+
+    final brickSpriteComponents = List.generate(
+      15,
+      (index) => SpriteComponent(
+        sprite: getMarioSprite(x: 373, y: 47, width: 16, height: 16),
+        position: Vector2(0 + index * 32, 82),
+        size: Vector2(32, 32),
+      ),
+    ).toList();
+    addAll(brickSpriteComponents);
+
     add(SpriteComponent(
-      sprite: upSprite,
-      position: Vector2(0, 0),
-      size: Vector2(680, 1252),
+      sprite: getMarioSprite(x: 23, y: 507, width: 13, height: 16),
+      position: Vector2(102, 34),
+      size: Vector2(39, 48),
     ));
   }
+}
+
+const String spriteImage = 'mario_sprites.gif';
+
+Sprite getMarioSprite({
+  required double x,
+  required double y,
+  required double width,
+  required double height,
+}) {
+  return Sprite(
+    Flame.images.fromCache(spriteImage),
+    srcPosition: Vector2(x, y),
+    srcSize: Vector2(width, height),
+  );
 }
